e62bd4f334553775aa1e9ff2e9b7252f01cb4e9f
msempere <odoncopon@gmail.com>
2014-12-26 18:27:39 +0000

    Preparation for travis and installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='easy_circus',
+            version='0.1',
+            description='Easy python ZMQ client and library for Mozilla Circus',
+            url='https://github.com/msempere/easy_circus',
+            author='msempere',
+            author_email='msempere@users.noreply.github.com',
+            license='MIT',
+            packages=['easy_circus'],
+            zip_safe=False)
